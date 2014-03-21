30c78654769d558769020f388e8d5136769ba3d3
irv <irvingthemagnificent@yahoo.com>
2014-03-21 19:20:10 -0400

    now possible to login. New signups have no role.

--- Gemfile
+++ Gemfile
@@ -36,10 +36,12 @@ end
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
 # Use unicorn as the app server
-# gem 'unicorn'
+gem 'unicorn'
 
 # Use Capistrano for deployment
 # gem 'capistrano', group: :development
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+
+gem 'devise'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+roles = Role.create([ { name: 'ceo', limit: 1000000000 }, # 1 billion
+                      { name: 'manager', limit: 10000 }, # 10 K
+                      { name: 'peon', limit: 100 }
+                    ])
+
+users = User.create([
+                      { email: 'ceo@xxerox.com', password: 'password', role_id: Role.all.first.id },
+                      { email: 'manager@xxerox.com', password: 'password', role_id: Role.all[1].id},
+                      { email: 'loser@xxerox.com', password: 'password', role_id: Role.all.last.id }
+                    ])
