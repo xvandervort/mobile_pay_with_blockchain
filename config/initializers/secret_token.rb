1ea41861a85d1b1adab4f2884b63c8c7e5a7e6c1
irv <irvingthemagnificent@yahoo.com>
2014-03-21 15:48:28 -0400

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MobilePayWithBlockchain::Application.config.secret_key_base = '664ae96ef349e6d7ed6570efd3c95ee1365257540da349f8c34cf202148b5ad3211efae4a12c911826c36d206aafd31fd68a3801c504ce0457f0dcccfae8dc18'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MobilePayWithBlockchain::Application.config.session_store :cookie_store, key: '_mobile_pay_with_blockchain_session'
